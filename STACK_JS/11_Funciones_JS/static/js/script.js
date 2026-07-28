b5c7672156ecfc3a3ed5648834835d7bf388c248
Xavier olmos <xavierolmos@liceovvh.cl>
2026-07-28 14:53:36 -0400

    HIIII

--- STACK_JS/11_Funciones_JS/static/js/script.js
+++ STACK_JS/11_Funciones_JS/static/js/script.js
@@ -1,2 +1,28 @@
 console.log("Conexión de js exitosa...");
 
+//Ejemplo funcion simple (Sin parametros)
+function saludar(/* parámetros */) {
+   alert("¡Hola, bienvenido!");
+}
+
+saludar(); //ejecucion de una función
+
+function Parametro(nombre) {
+   console.log("¡Hola, " + nombre + "!");
+}
+
+saludar("Luis");
+saludar("Ana");
+
+function encontrarMaximo(a, b) {
+   if (a > b) {
+       return a;
+   } else {
+       return b;
+   }
+}
+
+let numero1 = 10;
+let numero2 = 7;
+let maximo = encontrarMaximo(numero1, numero2);
+console.log("El número mayor entre", numero1, "y", numero2, "es:", maximo);
