1e4884ff3bc0bc545703579784bc9fc7236a0b4c
Xavier olmos <xavierolmos@liceovvh.cl>
2026-07-30 11:59:05 -0400

    Hala

--- STACK_JS/11_Funciones_JS/static/js/script.js
+++ STACK_JS/11_Funciones_JS/static/js/script.js
@@ -7,12 +7,19 @@ function saludar(/* parámetros */) {
 
 saludar(); //ejecucion de una función
 
-function Parametro(nombre) {
-   console.log("¡Hola, " + nombre + "!");
+//  👾Funciones con párametros
+//El parámetro recibe un valor para trabajar en la función
+//El parámetro recibe el tipo de dato el momento de tomar valor
+
+function saludarNombre(nombre) { //parámetro nombre
+   alert("¡Hola, " + nombre + "!");
 }
 
-saludar("Luis");
-saludar("Ana");
+saludarNombre("Xavier"); //ejecución de la funcion con parámetros (*argumentos*)
+saludarNombre("Alex");
+
+// 🧿Funciones con return
+function EncontrarMayor() {
 
 function encontrarMaximo(a, b) {
    if (a > b) {
@@ -24,5 +31,35 @@ function encontrarMaximo(a, b) {
 
 let numero1 = 10;
 let numero2 = 7;
-let maximo = encontrarMaximo(numero1, numero2);
-console.log("El número mayor entre", numero1, "y", numero2, "es:", maximo);
+let maximo = encontrarMaximo(numero1, numero2); // Maximo guardara el valor de retorno!!.
+alert(`El número mayor entre, ${numero1}, y, ${numero2},
+   \nes:, ${maximo}`);
+}
+
+//Tarea
+/*
+Crear una funcion que reciba 3 parametros, a, b y c. Debe sumar a + b y el resultado restarlo por c.
+Devolver el valor final y mostrar con un alert
+*/
+function mostrarNumeros() {
+    function calcularOpe(a, b, c) {
+    return (a + b) - c; 
+}
+function Calcular() {
+    //Creacion de variables
+    let Numero1 = parseInt(prompt("Ingrese Priimer Numero"));
+    let Numero2 = parseInt(prompt("Ingrese Segundo Numero"));
+    let Numero3 = parseInt(prompt("Ingrese Tercero Numero"));
+    //Muestra resultados
+    let REsultado = calcularOpe(Numero1, Numero2, Numero3);
+
+    alert(`El resultado de sumar ${Numero1} y ${Numero2}, y restarle ${Numero3}, \nes: ${REsultado}`);
+}
+
+Calcular();
+}
+
+/*
+Crear una funcion reciba un parámetro y permita a través de un bucle contar hasta este.
+Ej: Se recibe el número 5 y muestra: 1 - 2 - 3 - 4 - 5
+*/
